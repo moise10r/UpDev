217be8a473f8308fe08be0fc4979325640987ebb
moise10r <moiserushanika2006@gmail.com>
2024-04-04 04:11:00 +0200

    Bootstrap the layout.

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+import "../globals.css";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
+
+import { Questrial } from 'next/font/google';
+
+const inter = Questrial({
+  subsets: ['latin'],
+  weight: '400'
+})
+
+export const metadata: Metadata = {
+  title: "UpDev",
+  description: "Help Developers to tweet about new technologies",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
+        <body className={inter.className}>
+          <Topbar />
+
+          <main className='flex flex-row'>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
