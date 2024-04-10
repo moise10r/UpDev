0b1980f3530276b8859262e998ca15d27d5e29b5
moise10r <moiserushanika2006@gmail.com>
2024-04-10 04:11:00 +0200

    Implement the activities pages.

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const activity = await getActivity(userInfo._id);
+
+  return (
+    <div className="px-6">
+      <h1 className='head-text'>Activity</h1>
+
+      <section className='mt-10 flex flex-col gap-5'>
+        {activity.length > 0 ? (
+          <>
+            {activity.map((activity) => (
+              <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+                <article className='activity-card justify-between bg-[#ffffff0f] p-2 rounded-lg border border-[#ffffff23] hover:bg-[#ffffff16]'>
+                  <div className="flex items-center gap-2">
+                  <Image
+                    src={activity.author.image}
+                    alt='user_logo'
+                    width={20}
+                    height={20}
+                    className='rounded-full object-cover'
+                  />
+                  <p className='!text-small-regular text-light-1'>
+                    <span className='mr-1 text-primary-500'>
+                      {activity.author.name}
+                    </span>{" "}
+                    replied to your thread
+                  </p>
+                  </div>
+                  <div className="loader"></div>
+                </article>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p className='!text-base-regular text-light-3'>No activity yet</p>
+        )}
+      </section>
+    </div>
+  );
+}
+
+export default Page;
